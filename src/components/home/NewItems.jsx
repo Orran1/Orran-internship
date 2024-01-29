abe999a987996ee5f5e5a2ce45bc96ea13759582
orran <orran108@yahoo.co.uk>
2024-01-29 23:00:27 +0000

    animation done

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -31,7 +31,9 @@ const NewItems = () => {
         <div className="row">
           <div className="col-lg-12">
             <div className="text-center">
-              <h2>New Items</h2>
+              <h2 data-aos="fade-in" data-aos-anchor-placement="top-bottom">
+                New Items
+              </h2>
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
@@ -107,7 +109,11 @@ const NewItems = () => {
               }}
             >
               {newItem.map((item, index) => (
-                <div key={index}>
+                <div
+                  key={index}
+                  data-aos="fade-in"
+                  data-aos-anchor-placement="top-bottom"
+                >
                   <div className="nft__item">
                     <div className="author_list_pp">
                       <Link
